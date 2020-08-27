4db5afca47936c6b29e2413027c2e2a5a65f65a0
Victor Glindås <glinkis@gmail.com>
2020-08-27 21:12:04 +0200

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,11 @@
-import Head from "next/head"
 import styles from "../styles/Home.module.css"
-
-const AUTHOR_URL = "https://github.com/Glinkis"
-const PROJECT_URL = "https://github.com/Glinkis/trans-mini.git"
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href={PROJECT_URL} target="__blank" rel="noopener noreferrer">
-            trans-mini
-          </a>
-        </h1>
+        <h1 className={styles.title}>trans-mini</h1>
       </main>
-
-      <footer className={styles.footer}>
-        <a href={AUTHOR_URL} target="_blank" rel="noopener noreferrer">
-          Created by Glinkis
-        </a>
-      </footer>
     </div>
   )
 }
